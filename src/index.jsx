6ebaf49994a05ed7ec75e95764624eb5a1bc1ba6
Katsuhisa <n82_k86.vwl.ktc@kpb.biglobe.ne.jp>
2017-01-21 00:19:17 +0900

    redux-devtool-extensionの設定

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers';
 import ModalContainer from './container/ModalContainer';
 
-const store = createStore(rootReducer);
-
-store.subscribe(() => console.log(store.getState()));
+const store = createStore(
+  rootReducer,
+  {},
+  process.env.NODE_ENV === 'development' && window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 render(
   <Provider store={store}>
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,7 @@ module.exports = {
   },
 
   plugins: [
+    new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify('development') }),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
   ],
